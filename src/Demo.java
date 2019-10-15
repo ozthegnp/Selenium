ac2d87346ff6c77ed6955b533c20bb678f279e34
ozthgnp <oscaremateo@gmail.com>
2019-10-15 14:16:01 -0700

    add Chrome FireFox and Safari

--- src/Demo.java
+++ src/Demo.java
@@ -1,9 +1,21 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Demo {
+	
+	public static void main(String[] args) {				
+		
+		//invoke .exe file first
+		System.setProperty("webdriver.chrome.driver", "//Users//Oz//Documents//Selenium//drivers//chromedriver");
+		
+		//Create Driver Object for Chrome browser
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//selenium code-
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("http://www.google.com");
+		System.out.println(driver.getTitle());
+		driver.quit();
+		
 	}
 
 }
--- src/testcaseSafari.java
+++ src/testcaseSafari.java
@@ -1,21 +1,19 @@
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
-public class testCase2 {
-	
+public class testcaseSafari {
 	public static void main(String[] args) {				
 		
 		//invoke .exe file first for Firefox
-		System.setProperty("webdriver.gecko.driver", "//Users//Oz//Documents//Selenium//drivers//geckodriver");
+		//System.setProperty("webdriver.safari.driver", "//Users//Oz//Documents//Selenium//drivers//geckodriver");
 		
 		//Create Driver Object for Chrome browser
 
-		WebDriver driver = new FirefoxDriver();
+		WebDriver driver = new SafariDriver();
 		
 		driver.get("http://www.google.com");
 		System.out.println(driver.getTitle());
 		driver.quit();
 		
 	}
-
 }
