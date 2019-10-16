3df36d57fcf4c62ff282f257aa8e0b3af9ea077c
ozthgnp <oscaremateo@gmail.com>
2019-10-16 14:40:25 -0700

    finalize rediff testcase

--- src/testCaseRediffInChrome.java
+++ src/testCaseRediffInChrome.java
@@ -13,6 +13,10 @@ public class testCaseRediffInChrome {
 		WebDriver driver = new ChromeDriver();
 
 		driver.get("https://rediff.com");
+		driver.findElement(By.cssSelector("a[title *= 'Sign in']")).click();
+		driver.findElement(By.xpath("//input[@id = 'login1/paretn::div/input']")).sendKeys("myname");
+		driver.findElement(By.cssSelector("input#password")).sendKeys("1234");
+		driver.findElement(By.xpath("//input[contains( @name, 'procee')]")).click();
 		
 
 	}
