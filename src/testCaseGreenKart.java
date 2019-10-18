add808175408ea8de580e37eab13b32f37cf24b3
ozthgnp <oscaremateo@gmail.com>
2019-10-17 20:19:07 -0700

    explicit bugfix

--- src/testCaseGreenKart.java
+++ src/testCaseGreenKart.java
@@ -1,13 +1,13 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 public class testCaseGreenKart {
 	public static void main(String[] args) throws InterruptedException {
