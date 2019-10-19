3bdd543cb1cb6bceeb569ad54da38d25ba883919
ozthgnp <oscaremateo@gmail.com>
2019-10-18 20:35:12 -0700

    add comments

--- src/actions/actionsWindowHandleGoogle.java
+++ src/actions/actionsWindowHandleGoogle.java
@@ -13,32 +13,39 @@ import org.openqa.selenium.interactions.Actions;
 public class actionsWindowHandleGoogle {
 
 	public static void main(String[] args) {
-		
-			System.setProperty("webdriver.chrome.driver", "//Users//Oz//Documents//Selenium//drivers//chromedriver");
-	WebDriver driver = new ChromeDriver();
 
-	// Navigating to desired website
-	driver.get("http://accounts.google.com/signup");
-	
-	Actions chromeActions =  new Actions(driver);
-	
-	WebElement helpGoogleElement = driver.findElement(By.cssSelector(".RwBngc a"));
-	
-	helpGoogleElement.click();
-	System.out.println(driver.getTitle());
+		System.setProperty("webdriver.chrome.driver", "//Users//Oz//Documents//Selenium//drivers//chromedriver");
+		WebDriver driver = new ChromeDriver();
+
+		// Navigating to desired website
+		driver.get("http://accounts.google.com/signup");
+
+		Actions chromeActions = new Actions(driver);
+
+		WebElement helpGoogleElement = driver.findElement(By.cssSelector(".RwBngc a"));
+
+		helpGoogleElement.click();
+		System.out.println("Current window: " + driver.getTitle());
+
+		// creating window title set
+		Set<String> windowTitles = driver.getWindowHandles();
+
+		// using iterator to search between windows
+		Iterator<String> iterator = windowTitles.iterator();
+
+		// assigning value to windows
+		String parentId = iterator.next();
+		String childId = iterator.next();
+
+		// switching to child window
+		driver.switchTo().window(childId);
+		System.out.println("Switching to :" + driver.getTitle());
 		
-	Set<String> windowTitles = driver.getWindowHandles();
-	
-	Iterator<String> iterator = windowTitles.iterator();
-	
-	String parentId = iterator.next();
-	String childId = iterator.next();
-	
-	driver.switchTo().window(childId);
-	
-	System.out.println(driver.getTitle());	
-	
-	//chromeActions.moveToElement(helpGoogleElement).click().keyDown(Keys.SHIFT).sendKeys("hello").doubleClick().build().perform();
+		// switching back to parent window
+		driver.switchTo().window(parentId);
+		System.out.println("Switching to :" + driver.getTitle());
+
+		// chromeActions.moveToElement(helpGoogleElement).click().keyDown(Keys.SHIFT).sendKeys("hello").doubleClick().build().perform();
 	}
 
 }
