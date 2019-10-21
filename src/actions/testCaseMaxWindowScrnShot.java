71c8a36794b0e2a988e2fb7bf59f29e52a8f7da5
ozthgnp <oscaremateo@gmail.com>
2019-10-21 00:03:35 -0700

    add screenshot

---
+++ src/actions/testCaseMaxWindowScrnShot.java
@@ -0,0 +1,31 @@
+package actions;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+
+public class testCaseMaxWindowScrnShot {
+
+	public static void main(String[] args) throws IOException {
+		System.setProperty("webdriver.chrome.driver", "//Users//Oz//Documents//Selenium//drivers//chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		
+		// Navigating to desired website
+		driver.get("https://google.com");
+		
+		File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src, new File("//Users//Oz//Documents//Selenium//screenshots//cssht.png"));
+		
+
+	}
+
+}
