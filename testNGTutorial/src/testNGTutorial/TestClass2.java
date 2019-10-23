954c93237d54f584f72f6ac6b3135c53e73b516e
ozthgnp <oscaremateo@gmail.com>
2019-10-23 00:23:19 -0700

    exten TC2 with TC4 class

--- testNGTutorial/src/testNGTutorial/TestClass2.java
+++ testNGTutorial/src/testNGTutorial/TestClass2.java
@@ -1,10 +1,12 @@
 package testNGTutorial;
 
+import java.io.IOException;
+
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class TestClass2 {
+public class TestClass2 extends TestClass4{
 	@AfterSuite
 	public void afterSuite() {
 		System.out.print("TestClass2: ");
@@ -12,7 +14,8 @@ public class TestClass2 {
 	}
 	
 	@Test(groups= {"Smoke"})
-	public void TC1() {
+	public void TC1() throws IOException, InterruptedException {
+		Login();
 		System.out.print("TestClass2: ");
 		System.out.println("Hello in test class 2");
 	}
