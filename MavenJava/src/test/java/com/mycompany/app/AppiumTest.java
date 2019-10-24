9fb96b159a773f617a210a0017640c91b266694d
ozthgnp <oscaremateo@gmail.com>
2019-10-23 19:17:35 -0700

    add dummy testcases

---
+++ MavenJava/src/test/java/com/mycompany/app/AppiumTest.java
@@ -0,0 +1,17 @@
+package com.mycompany.app;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void NativeAppAdroid() {
+		
+	}
+	
+	@Test
+	public void IOSApps() {
+		
+	}
+
+}
---
+++ MavenJava/src/test/java/com/mycompany/app/RESTAPITest.java
@@ -0,0 +1,16 @@
+package com.mycompany.app;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	
+	@Test
+	public void postJira() {
+		
+	}
+	
+	@Test
+	public void deleteTwitter() {
+		
+	}
+}
