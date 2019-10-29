2b0aa104a8364cc5c2a962d5d805e50470d1a290
ozthgnp <oscaremateo@gmail.com>
2019-10-29 16:17:44 -0700

    update Testcase with dataProviders

--- DemoProject/src/test/java/DemoGroup/HomePage.java
+++ DemoProject/src/test/java/DemoGroup/HomePage.java
@@ -2,23 +2,49 @@ package DemoGroup;
 
 import java.io.IOException;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import pageObjects.LandingPage;
+import pageObjects.LoginPage;
 import resources.Base;
 
-public class HomePage extends Base{
-	
-	@Test
-	public void basePageNavigation() throws IOException {
+public class HomePage extends Base {
+
+	@Test(dataProvider = "getData")
+	public void basePageNavigation(String username, String password, String text) throws IOException {
 		driver = initDriver();
-		
+
 		driver.get("http://www.qaclickacademy.com/");
-		
+
 		LandingPage l = new LandingPage(driver);
-		
 		l.getLogin().click();
-		
-		}
+
+		LoginPage lp = new LoginPage(driver);
+		lp.getEmail().sendKeys(username);
+		lp.getPassword().sendKeys(password);
+
+		System.out.println(text);
+		lp.getSubmit().click();
+
+	}
+
+	@DataProvider
+	public Object[][] getData() {
+		Object[][] data = new Object[2][3];
+
+		// 0th row
+		data[0][0] = "restricted.user@gmail.com";
+		data[0][1] = "12345";
+		data[0][2] = "Resticted User";
+
+		// 1th row
+		data[1][0] = "non.restricted.user@gmail.com";
+		data[1][1] = "45678";
+		data[1][2] = "Non-Resticted User";
+
+		return data;
+
+	}
 
 }
