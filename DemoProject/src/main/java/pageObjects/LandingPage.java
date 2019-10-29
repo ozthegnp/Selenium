3eee1ce7167e9926dab1f7d191ce2ef411f60fba
ozthgnp <oscaremateo@gmail.com>
2019-10-29 16:21:19 -0700

    adding title element

--- DemoProject/src/main/java/pageObjects/LandingPage.java
+++ DemoProject/src/main/java/pageObjects/LandingPage.java
@@ -7,22 +7,30 @@ import org.openqa.selenium.WebElement;
 import resources.Base;
 
 public class LandingPage {
-	
+
 	public WebDriver driver;
-	
+
 	Base b = new Base();
-	
+
 	public LandingPage(WebDriver driver) {
 		this.driver = driver;
 	}
-	
-	
+
+
 	By signInLink = By.cssSelector("a[href*= 'sign_in']");
-	
+	By title = By.cssSelector("div.container > h2:nth-child(1)']");
+
+
+
 	public  WebElement getLogin() {
-		
+
 		return driver.findElement(signInLink);
 	}
-	
+
+	public  WebElement getTitle() {
+
+		return driver.findElement(title);
+	}
+
 
 }
